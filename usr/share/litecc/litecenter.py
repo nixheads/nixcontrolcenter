af6de31fc5a91ea0ca2f72fab2bc16dfea7c5dc5
Johnathan Jenkins <john@nixheads.co.uk>
2016-02-20 07:33:30 -0800

    tweaking

--- usr/share/litecc/litecenter.py
+++ usr/share/litecc/litecenter.py
@@ -10,7 +10,8 @@ from gi.repository import Gtk as gtk
 from gi.repository.GdkPixbuf import Pixbuf
 gi.require_version('WebKit', '3.0')
 from gi.repository import WebKit as webkit
-import threading, multiprocessing
+import threading
+import multiprocessing
 
 app_dir = '/usr/share/litecc'
 
@@ -316,10 +317,10 @@ def frontend_fill():
 def main():
     global browser
     global window
-    #frontend = frontend_fill()
+    # frontend = frontend_fill()
     for i in range(multiprocessing.cpu_count()):
         t = threading.Thread(target=frontend_fill)
-		t.start()
+        t.start()
     window = gtk.Window()
     window.connect('destroy', gtk.main_quit)
     window.set_title("Linux Lite Control Center")

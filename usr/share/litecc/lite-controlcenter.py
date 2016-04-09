7a1a1ee43bd0148f2f369c351b449e0abf201203
Johnathan Jenkins <john@nixheads.co.uk>
2016-04-09 00:10:43 -0700

    [linting] over indented

--- usr/share/litecc/lite-controlcenter.py
+++ usr/share/litecc/lite-controlcenter.py
@@ -215,14 +215,16 @@ def get_info(info):
             count = apt_info()
 
             if os.path.isfile(pkgcache):
-                            mtime = os_stat(pkgcache).st_mtime
-                            modtime = datetime.datetime.fromtimestamp(mtime).strftime('%Y-%m-%d %H:%M')
-                            modday = datetime.datetime.fromtimestamp(mtime).strftime('%Y-%m-%d')
-                            today = datetime.datetime.today().strftime('%Y-%m-%d')
-                            if modday == today:
-                                updaters = '''<section class="gradient">Last checked on <font style=\"color: green;\">{0}</font>, <font style=\"color: red;\">{1} </font> updates available.<button style=\"padding-bottom:0px;padding-left:50pxi\" onclick=\"location.href=('update://')\">Run Updates</button></section>'''.format(modtime, count)
-                            else:
-                                updaters = '''<section class="gradient">Last checked on <font style=\"color: red;\">{0}</font>, <font style=\"color: red;\">{1} </font> updates available.<button style=\"padding-bottom:0px;padding-left:50pxi\" onclick=\"location.href=('update://')\">Run Updates</button></section>'''.format(modtime, count)
+                mtime = os_stat(pkgcache).st_mtime
+                modtime = datetime.datetime.fromtimestamp(mtime).strftime('%Y-%m-%d %H:%M')
+                modday = datetime.datetime.fromtimestamp(mtime).strftime('%Y-%m-%d')
+                today = datetime.datetime.today().strftime('%Y-%m-%d')
+                if modday == today:
+                    updaters = '''<section class="gradient">Last checked on <font style=\"color: green;\">{0}</font>, <font style=\"color: red;\">{1} </font> updates available.<button style=\"padding-bottom:0px;padding-left:50pxi\" onclick=\"location.href=('update://')\">Run Updates</button></section>'''.format(
+                        modtime, count)
+                else:
+                    updaters = '''<section class="gradient">Last checked on <font style=\"color: red;\">{0}</font>, <font style=\"color: red;\">{1} </font> updates available.<button style=\"padding-bottom:0px;padding-left:50pxi\" onclick=\"location.href=('update://')\">Run Updates</button></section>'''.format(
+                        modtime, count)
             else:
                 updaters = '''<section class="gradient">No Update History<button style=\"padding-bottom:0px;padding-left:50pxi\" onclick=\"location.href=('update://')\">Run Updates</button></section>'''
 
